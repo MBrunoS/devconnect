8b0e4d179622b4ce1ee54690c32bfe2806c91dce
Maurício Bruno <mb.bruno.s@gmail.com>
2023-01-06 17:34:37 -0300

    feat: enable comments deletion

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import { useFormattedDates } from "../hooks/useFormattedDates";
+import { api } from "../services/api";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
-export function Comment({ content, author, publishedAt }) {
+export function Comment({ id, content, author, publishedAt, onUpdate }) {
   const { formatted, relativeToNow } = useFormattedDates(publishedAt);
+
+  function handleCommentDeletion() {
+    api.delete("comments", { data: { id } });
+    onUpdate();
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar src={author.avatarUrl} hasBorder={false} />
@@ -19,7 +26,7 @@ export function Comment({ content, author, publishedAt }) {
               </time>
             </div>
 
-            <button title="Delete comment">
+            <button title="Delete comment" onClick={handleCommentDeletion}>
               <Trash size={24} />
             </button>
           </header>
