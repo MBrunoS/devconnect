e015a3e9b490f8ac8c5aa635c9a47e31a0ef91ab
Maurício Bruno <mb.bruno.s@gmail.com>
2023-01-12 19:32:11 -0300

    feat: update dates locale

--- src/hooks/useFormattedDates.ts
+++ src/hooks/useFormattedDates.ts
@@ -1,13 +1,9 @@
 import { format, formatDistanceToNow, parseISO } from "date-fns";
-import { ptBR } from "date-fns/locale";
 
 export const useFormattedDates = (dateISOString) => {
   const date = parseISO(dateISOString);
-  const publishedDateFormatted = format(date, "d 'de' LLLL 'às' HH:mm'h'", {
-    locale: ptBR,
-  });
+  const publishedDateFormatted = format(date, "d LLLL 'at' HH:mm");
   const publishedDateRelativeToNow = formatDistanceToNow(date, {
-    locale: ptBR,
     addSuffix: true,
   });
 
